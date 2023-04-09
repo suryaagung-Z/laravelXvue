4c62fdc5bd5c6258ac4b7b6ebabb52f76775281a
suryaagung-Z <suryaagung118@gmail.com>
2023-04-10 00:31:02 +0700

    repair

--- resources/views/templates/sections/navbar.blade.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <ul style="display:flex; gap: 20px;">
-        <li style="list-style-type: none;"><a href="/">Home</a></li>
-        <li style="list-style-type: none;"><a href="/product">Product</a></li>
-        <li style="list-style-type: none;"><a href="/gallery">Gallery</a></li>
-        <li style="list-style-type: none;"><a href="/contact">Contact</a></li>
-    </ul>
-</nav>
